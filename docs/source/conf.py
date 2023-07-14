864537431afe0a5b63e4fe491bdd996f648efad1
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-07-14 16:39:05 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,14 +11,7 @@ version = '0.1.0'
 
 # -- General configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinxcontrib.matlab',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
+extensions = ['sphinxcontrib.matlab', 'sphinx.ext.autodoc']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
