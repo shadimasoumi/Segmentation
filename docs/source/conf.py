0957ff614dd1d86cc07ed993a9afefea936727a3
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-07-14 15:20:38 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
+    'sphinxcontrib.matlab',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
