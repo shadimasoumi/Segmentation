6222e078d945b5d2d3fe8a48ff149b8d99486fca
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-07-14 11:17:02 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Segmentation'
+copyright = '2023, Shadi'
+author = 'Shadi'
 
 release = '0.1'
 version = '0.1.0'
