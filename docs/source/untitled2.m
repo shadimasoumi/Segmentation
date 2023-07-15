998bd20a87f89367e37f7b73f74042e60f1128c1
Shadi Masoumi <shadimasoumiphd@gmail.com>
2023-07-15 16:05:36 -0400

    Update untitled2.m

--- docs/source/untitled2.m
+++ docs/source/untitled2.m
@@ -1,14 +1,7 @@
-%% Vector Operations
-% You can perform a number of binary operations on vectors.
-%%
-A = 1:3;
-B = 4:6;
-%% Dot Product
-% A dot product of two vectors yields a scalar.  
-% MATLAB has a simple command for dot products.
-s = dot(A,B);
-%% Cross Product
-% A cross product of two vectors yields a third
-% vector perpendicular to both original vectors.
-% Again, MATLAB has a simple command for cross products.
-v = cross(A,B);
+function y = times_two(x)
+% times_two Multiply a number by two
+%
+% :param x: The input value
+% :returns: The input value multiplied by two
+
+y = x * 2;
